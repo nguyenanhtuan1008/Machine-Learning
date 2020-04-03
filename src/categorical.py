bb62ec1a761cd3e9ff37c3c28963f212c23f6e2a
nguyenanhtuan1008 <nguyenanhtuan1008@gmail.com>
2020-04-03 18:21:16 +0900

    binary

--- src/categorical.py
+++ src/categorical.py
@@ -20,6 +20,7 @@ class CategoricalFeatures:
         self.cat_feats = categorical_features
         self.enc_type = encoding_type
         self.label_encoders = dict()
+        self.binary_encoders = dict()
 
         if handle_na:
             for c in self.cat_feats:
@@ -32,23 +33,60 @@ class CategoricalFeatures:
             self.output_df.loc[:, c] = lbl.transform(self.df[c].values)
             self.label_encoders[c] = lbl
         return self.output_df
-    def transform(self):
+
+    def _label_binarization(self):
+        for c in self.cat_feats:
+            lbl = preprocessing.LabelBinarizer()
+            lbl.fit(self.df[c].values)
+            val = lbl.transform(self.df[c].values) # array
+            self.output_df = self.output_df.drop(c, axis=1)
+            for j in range(val.shape[1]):
+                new_col_name = c + f"__bin__{j}"
+                self.output_df[new_col_name] = val[:, j]
+            self.binary_encoders[c] = lbl
+        return self.output_df
+            
+
+    def fit_transform(self):
         if self.enc_type == "label":
             return self._label_encoding()
+        elif self.enc_type == "binary":
+            return self._label_binarization()
+        # elif self.enc_type == "ohe":
+        #     return self._one_hot()
         else:
             raise Exception("Encoding type not understood")
 
+    def transform(self, dataframe):
+        if self.handle_na:
+            for c in self.cat_feats:
+                dataframe.loc[:, c] = dataframe.loc[:, c].astype(str).fillna("-9999999")
+
+        if self.enc_type == "label":
+            for c, lbl in self.label_encoders.items():
+                dataframe.loc[:, c] = lbl.transform(dataframe[c].values)
+            return dataframe
+
+        if self.enc_type == "binary":
+            for c, lbl in self.label_encoders.items():
+                val = lbl.transform(dataframe[c].values)
+                dataframe = dataframe.drop(c, axis=1)
+
+                for j in range(val.shape[1]):
+                    new_col_name = c + f"__bin__{j}"
+                    dataframe[new_col_name] = val[:, j]
+            return dataframe
 
 if __name__ == "__main__":
     import pandas as pd
-    df = pd.read_csv("../input/train_cat.csv")
+    df = pd.read_csv("../input/train_cat.csv").head(500)
     cols = [c for c in df.columns if c not in ["id", "target"]]
     print(cols)
     cat_feats = CategoricalFeatures(df, 
                                     categorical_features=cols,
-                                    encoding_type="label",
+                                    encoding_type="binary",
                                     handle_na=True)
-    output_df = cat_feats.transform()
+    output_df = cat_feats.fit_transform()
     print(output_df.head())
 
     
